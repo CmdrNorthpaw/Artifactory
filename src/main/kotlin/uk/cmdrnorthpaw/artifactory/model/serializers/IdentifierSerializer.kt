2d259a422a59796129255f0efd13ecc562307418
CmdrNorthpaw <thetuxkid@gmail.com>
2021-05-08 12:08:36 +0100

    Change IdentifierSerializer.kt to be a primitive serializer.

--- src/main/kotlin/uk/cmdrnorthpaw/artifactory/model/serializers/IdentifierSerializer.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/artifactory/model/serializers/IdentifierSerializer.kt
@@ -3,26 +3,22 @@ package uk.cmdrnorthpaw.artifactory.model.serializers
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import net.minecraft.util.Identifier
 
 object IdentifierSerializer : KSerializer<Identifier> {
-
-    @Serializable
-    @JvmInline
-    private value class SerializableIdentifier(val identifier: String)
-
-    override val descriptor: SerialDescriptor = SerializableIdentifier.serializer().descriptor
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Identifier", PrimitiveKind.STRING)
 
     override fun deserialize(decoder: Decoder): Identifier {
-        val serializableIdentifier = decoder.decodeSerializableValue(SerializableIdentifier.serializer())
-        return Identifier.tryParse(serializableIdentifier.identifier) ?: throw SerializationException("Invalid identifier decoded!")
+        return Identifier.tryParse(decoder.decodeString()) ?: throw SerializationException("Invalid identifier decoded!")
     }
 
     override fun serialize(encoder: Encoder, value: Identifier) {
-        encoder.encodeSerializableValue(SerializableIdentifier.serializer(), SerializableIdentifier(value.toString()))
+        encoder.encodeString(value.toString())
     }
 
     fun Identifier.serializer() = this@IdentifierSerializer
