bcd8f87dbb6337fbb9de00d424c37a774b181b9f
CmdrNorthpaw <thetuxkid@gmail.com>
2021-05-18 17:28:56 +0100

    Use Ingredients in CookingRecipe

--- src/main/kotlin/uk/cmdrnorthpaw/artifactory/model/recipies/CookingRecipe.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/artifactory/model/recipies/CookingRecipe.kt
@@ -1,11 +1,12 @@
 package uk.cmdrnorthpaw.artifactory.model.recipies
 
 import kotlinx.serialization.SerialName
+import uk.cmdrnorthpaw.artifactory.model.Ingredient
 import uk.cmdrnorthpaw.artifactory.model.serializers.Identifier
 
 abstract class CookingRecipe : Recipe() {
     @SerialName("ingredient")
-    abstract val ingredients: List<Identifier>
+    abstract val ingredients: List<Ingredient>
 
     abstract val result: Identifier
 
