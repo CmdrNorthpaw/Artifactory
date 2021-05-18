5a88978ca8dda62cc75e66d73e2eb593c2a549c0
CmdrNorthpaw <thetuxkid@gmail.com>
2021-05-18 16:35:49 +0100

    Builder for ShapedCrafting.kt

--- src/main/kotlin/uk/cmdrnorthpaw/artifactory/Artifactory.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/artifactory/Artifactory.kt
@@ -1,7 +1,13 @@
 package uk.cmdrnorthpaw.artifactory
 
 import net.fabricmc.api.ModInitializer
+import net.minecraft.item.Item
+import net.minecraft.util.registry.Registry
+import uk.cmdrnorthpaw.artifactory.model.serializers.Identifier
 
 object Artifactory : ModInitializer {
     override fun onInitialize() {}
 }
+
+val Item.identifier: net.minecraft.util.Identifier
+    get() = Registry.ITEM.getId(this)
--- src/main/kotlin/uk/cmdrnorthpaw/artifactory/recipes/ShapedCrafting.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/artifactory/recipes/ShapedCrafting.kt
@@ -2,6 +2,8 @@ package uk.cmdrnorthpaw.artifactory.recipes
 
 import kotlinx.serialization.Required
 import kotlinx.serialization.Serializable
+import net.minecraft.item.Item
+import uk.cmdrnorthpaw.artifactory.identifier
 import uk.cmdrnorthpaw.artifactory.model.RecipeResult
 import uk.cmdrnorthpaw.artifactory.model.recipies.Recipe
 import uk.cmdrnorthpaw.artifactory.model.recipies.RecipeType
@@ -22,5 +24,30 @@ class ShapedCrafting private constructor(
         assert(this.pattern.size <= 3)
         assert(pattern.none { it.length != 3 })
         assert(key.none { it.first == ' ' })
+    }
+
+
+    class Builder(val result: Item, val amount: Int = 1) {
+        private val pattern: MutableList<String> = mutableListOf()
+        private val key: MutableList<Pair<Char, Identifier>> = mutableListOf()
+        private var group: String = ""
+
+        fun patternLine(line: String): Builder {
+            assert(line.length == 3)
+            pattern.add(line)
+            return this
+        }
+
+        fun key(key: Char, value: Identifier): Builder {
+            this.key.add(key to value)
+            return this
+        }
+
+        fun setGroup(group: String): Builder {
+            this.group = group
+            return this
+        }
+
+        fun build(): ShapedCrafting = ShapedCrafting(pattern, key, RecipeResult(result.identifier, amount), group)
     }
 }
